07fbaa62191ae249ce69e536e72ee4a71e2b9cf3
Fredrik <jogfre@gmail.com>
2024-05-22 22:23:56 +0200

    Tweaked the progressbar to scale better

--- src/app/Pages/ProjectSection.jsx
+++ src/app/Pages/ProjectSection.jsx
@@ -36,7 +36,7 @@ const ProjectPage = ({scaleHook}) => {
   const scale = useTransform(
       scrollYProgress,
       [0, 1],
-      [0, 0.25],
+      [0, 0.20],
   )
 
   const smoothScale = useSpring(scale, {
